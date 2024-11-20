565ee2ad235a6940a3bbf1f8a7c55a1719170373
Mihret Bekure <mihretbekure12@gmail.com>
2024-11-20 01:47:05 -0800

    added axios
    add axios to the html head

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temprature");
+  temperatureElement.innerHtml = searchInput.value;
+}
+function searchCity(city) {
+  let apikey = "764t30of3bb7082cb08301ec7c1324a9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}& units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
