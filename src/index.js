dc1e45f4f8e3d86cf2fa615cd372d5b4978b0a23
Mihret Bekure <mihretbekure12@gmail.com>
2024-11-22 07:29:22 -0800

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temprature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let discriptionElement = document.querySelector("#discription");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  discriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "764t30of3bb7082cb08301ec7c1324a9";
